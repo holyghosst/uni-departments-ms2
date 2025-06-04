f16aed825794c423709268c7e3012063e3485233
Denys Mohylov <d.mohylov@gmail.com>
2025-06-04 23:51:31 +0200

    db integration added + tables fetching implemented

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import tableRoutes from './routes/tableRoutes';
 import dotenv from 'dotenv';
-import clientRoutes from './routes/clients';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/clients', clientRoutes);
+app.use('/api', tableRoutes);
 
-app.listen(PORT, () => {
-  console.log(`server is running at http://localhost:${PORT}`);
+const HOST = process.env.HOST || 'localhost';
+const PORT = Number(process.env.PORT) || 5000;
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`);
 });
